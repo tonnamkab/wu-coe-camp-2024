f0196f42fcde8ae258e3b0c6853f88fc167a8d98
tonnam <153060468+tonnamkab@users.noreply.github.com>
2024-06-22 15:21:44 +0700

    Initial commit

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,11 @@
+
+export default function Blog() {
+    return(
+        <div className="flex justify-center">
+            <div className="m-6 h-[100px] w-[200px] text-center rounded-lg border-2 border-inherit bg-slate-200">
+                <div>title</div>
+                <div>details</div>
+            </div>
+        </div>
+    )
+}
